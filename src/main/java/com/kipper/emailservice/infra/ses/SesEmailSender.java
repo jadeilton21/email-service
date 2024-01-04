ba4b35bbc533d36f56354867d1495c24e2c434a5
jadeilton <jadeiltondm@gmail.com>
2024-01-04 14:50:51 -0300

    feat: create class SesEmailSender

---
+++ src/main/java/com/kipper/emailservice/infra/ses/SesEmailSender.java
@@ -0,0 +1,22 @@
+package com.kipper.emailservice.infra.ses;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.kipper.emailservice.adapters.EmailSenderGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+
+    }
+}
