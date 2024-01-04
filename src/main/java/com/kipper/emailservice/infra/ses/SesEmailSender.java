a959f577547fbdc26d82a1a3ffe9a4bd57028577
jadeilton <jadeiltondm@gmail.com>
2024-01-04 15:17:56 -0300

    feat: import method.

--- src/main/java/com/kipper/emailservice/infra/ses/SesEmailSender.java
+++ src/main/java/com/kipper/emailservice/infra/ses/SesEmailSender.java
@@ -1,7 +1,10 @@
 package com.kipper.emailservice.infra.ses;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
 import com.kipper.emailservice.adapters.EmailSenderGateway;
+import com.kipper.emailservice.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,21 @@ public class SesEmailSender implements EmailSenderGateway {
 
     @Override
     public void sendEmail(String to, String subject, String body) {
+
+
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("livesjaderKipper@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withHtml(new Content(body))));
+
+
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException exception) {
+            throw new EmailServiceException("Failure white sending email", exception);
+        }
 
     }
 }
