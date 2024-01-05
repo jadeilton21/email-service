b7a704290ac4e84f6f3b2f83ce3078176d399c0e
jadeilton <jadeiltondm@gmail.com>
2024-01-05 10:27:10 -0300

    feat: update class EmailSenderController

--- src/main/java/com/kipper/emailservice/controllers/EmailSenderController.java
+++ src/main/java/com/kipper/emailservice/controllers/EmailSenderController.java
@@ -7,9 +7,7 @@ import com.kipper.emailservice.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
@@ -24,6 +22,8 @@ public class EmailSenderController {
     }
 
 
+
+    @PostMapping 
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
 
         try{
