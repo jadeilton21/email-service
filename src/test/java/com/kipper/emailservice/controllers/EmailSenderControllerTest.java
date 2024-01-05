2ff12df12a5491b1aa4441a93c29023a80dbadf2
jadeilton <jadeiltondm@gmail.com>
2024-01-05 16:34:37 -0300

    feat: create test method sendEmail();

--- src/test/java/com/kipper/emailservice/controllers/EmailSenderControllerTest.java
+++ src/test/java/com/kipper/emailservice/controllers/EmailSenderControllerTest.java
@@ -1,7 +1,23 @@
 package com.kipper.emailservice.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class EmailSenderControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void sendEmail() {
+    }
 }
