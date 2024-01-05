1108a80e2279c0b3826d401558b1cf083d9a95b6
jadeilton <jadeiltondm@gmail.com>
2024-01-05 10:26:59 -0300

    feat: update class AwsSesConfig

--- src/main/java/com/kipper/emailservice/infra/ses/AwsSesConfig.java
+++ src/main/java/com/kipper/emailservice/infra/ses/AwsSesConfig.java
@@ -1,22 +1,31 @@
 package com.kipper.emailservice.infra.ses;
 
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfig {
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
 
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
 
     @Bean
-    public AmazonSimpleEmailService amazonSimpleEmailService(){
-
-        return AmazonSimpleEmailServiceClientBuilder.standard().build();
-
-
-
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKeyId, secretKey)))
+                .withRegion(awsRegion)
+                .build();
     }
 
 
