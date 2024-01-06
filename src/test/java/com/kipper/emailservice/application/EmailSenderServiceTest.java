98712cb6a6cf2be3ca8529855bc24ec2f6cea069
jadeilton <jadeiltondm@gmail.com>
2024-01-06 17:30:12 -0300

    feat: add MockMvc.

--- src/test/java/com/kipper/emailservice/application/EmailSenderServiceTest.java
+++ src/test/java/com/kipper/emailservice/application/EmailSenderServiceTest.java
@@ -1,7 +1,9 @@
 package com.kipper.emailservice.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class EmailSenderServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
