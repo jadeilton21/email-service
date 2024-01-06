e89976b6797814890e9a4e4c82a1643beaf020c1
jadeilton <jadeiltondm@gmail.com>
2024-01-06 17:55:49 -0300

    feat: create test method sendEmail.

--- src/test/java/com/kipper/emailservice/application/EmailSenderServiceTest.java
+++ src/test/java/com/kipper/emailservice/application/EmailSenderServiceTest.java
@@ -1,5 +1,6 @@
 package com.kipper.emailservice.application;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,7 @@ class EmailSenderServiceTest {
     private MockMvc mockMvc;
 
 
-
-
+    @Test
+    void sendEmail() {
+    }
 }
