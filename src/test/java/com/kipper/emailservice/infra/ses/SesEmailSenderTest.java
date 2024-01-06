52c532c505853259b71991ca2ca73bb83a634c25
jadeilton <jadeiltondm@gmail.com>
2024-01-06 18:09:57 -0300

    feat: add MockMvc

---
+++ src/test/java/com/kipper/emailservice/infra/ses/SesEmailSenderTest.java
@@ -0,0 +1,17 @@
+package com.kipper.emailservice.infra.ses;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SesEmailSenderTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
