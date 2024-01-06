1248971ef58ea2e76d969cbff96b0dde6dffffba
jadeilton <jadeiltondm@gmail.com>
2024-01-06 18:02:18 -0300

    feat: create method test amazonSimpleEmailService.

--- src/test/java/com/kipper/emailservice/infra/ses/AwsSesConfigTest.java
+++ src/test/java/com/kipper/emailservice/infra/ses/AwsSesConfigTest.java
@@ -1,5 +1,6 @@
 package com.kipper.emailservice.infra.ses;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,4 +18,8 @@ class AwsSesConfigTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+    @Test
+    void amazonSimpleEmailService() {
+    }
 }
