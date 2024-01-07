5ec0a54cb38f188ab995352e6366f6020e875e63
jadeilton <jadeiltondm@gmail.com>
2024-01-07 16:42:13 -0300

    feat: method sendEmail_cenario1, tested.

--- src/main/java/com/kipper/emailservice/infra/ses/SesEmailSender.java
+++ src/main/java/com/kipper/emailservice/infra/ses/SesEmailSender.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.kipper.emailservice.adapters.EmailSenderGateway;
 import com.kipper.emailservice.core.exceptions.EmailServiceException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,30 +14,27 @@ public class SesEmailSender implements EmailSenderGateway {
 
     private final AmazonSimpleEmailService amazonSimpleEmailService;
 
-
     @Autowired
     public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
         this.amazonSimpleEmailService = amazonSimpleEmailService;
     }
 
-
     @Override
     public void sendEmail(String to, String subject, String body) {
-
-
-        SendEmailRequest request = new SendEmailRequest()
-                .withSource("jadeiltondm@gmail.com")
-                .withDestination(new Destination().withToAddresses(to))
-                .withMessage(new Message()
-                        .withSubject(new Content(subject))
-                        .withBody(new Body().withHtml(new Content(body))));
-
-
         try {
-            this.amazonSimpleEmailService.sendEmail(request);
-        } catch (AmazonServiceException exception) {
-            throw new EmailServiceException("Failure white sending email", exception);
-        }
+            SendEmailRequest request = new SendEmailRequest()
+                    .withSource("jadeiltondm@gmail.com")
+                    .withDestination(new Destination().withToAddresses(to))
+                    .withMessage(new Message()
+                            .withSubject(new Content(subject))
+                            .withBody(new Body().withHtml(new Content(body))));
 
+            this.amazonSimpleEmailService.sendEmail(request);
+
+
+        } catch (AmazonServiceException exception) {
+
+            throw new EmailServiceException("Falha ao enviar email", exception);
+        }
     }
 }
