215bb34e3716a4e1f3ec2a53902772af45226b60
jadeilton <jadeiltondm@gmail.com>
2024-01-08 15:03:32 -0300

    feat: method sendEmail_cenario1, tested.

--- src/main/java/com/kipper/emailservice/application/EmailSenderService.java
+++ src/main/java/com/kipper/emailservice/application/EmailSenderService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailSenderService  implements EMailSenderUserCase {
 
-
     private final EmailSenderGateway eMailSenderGateway;
     @Autowired
     public EmailSenderService(EmailSenderGateway eMailSenderGatewat){
